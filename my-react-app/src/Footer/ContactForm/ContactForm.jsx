e309b7c6d795e1139376ebfe7e48e6379502601f
Magnitoshka <92590449+Magnitoshka@users.noreply.github.com>
2025-11-12 09:55:26 +0200

    ContactForm

--- my-react-app/src/Footer/ContactForm/ContactForm.jsx
+++ my-react-app/src/Footer/ContactForm/ContactForm.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import "./ContactForm.css"
 import LongButton from "/src/Components/LongButton/LongButton.jsx";
 
 export default function ContactForm() {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        formData.append("access_key", "14cb941a-5ad9-4ed2-b1e1-2c4c59fe1b42");
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+        setResult(data.success ? "Success!" : "Error");
+    };
+
     return (
         <section className="ContactForm">
-            <form className="footer-form">
+            <form onSubmit={onSubmit} className="footer-form">
                 <div className="input-box">
-                    <label className="body-medium">Name</label>
-                    <input type="text" className="field" placeholder="Palkin Oleg" required/>
+                    <label className="body-medium">Name</label >
+                    <input type="text" name="name" className="field" placeholder="Palkin Oleg" required/>
                 </div>
                 <div className="input-box">
                     <label className="body-medium">Email</label>
-                    <input type="text" className="field" required/>
+                    <input type="text" name="email" className="field" required/>
                 </div>
                 <div className="input-box">
                     <label className="body-medium">Subject</label>
@@ -19,11 +37,12 @@ export default function ContactForm() {
                 </div>
                 <div className="input-box">
                     <label className="body-medium">Message</label>
-                    <input type="text" className="field field-large" required/>
+                    <textarea name="message" className="field field-large" required/>
                 </div>
                 <div className="footer-submit-container">
                     <LongButton type="submit" label="SUBMIT"/>
                 </div>
+                <p>{result}</p>
             </form>
         </section>
     )
--- my-react-app/src/Footer/Footer.jsx
+++ my-react-app/src/Footer/Footer.jsx
@@ -6,14 +6,13 @@ function Footer() {
         <div className="footer">
             <div className="footer-container">
                 <div className="footer-nav-container">
-                    <h1 className="footer-label">LET`S CONNECT</h1>
-                    <h3>Say hello at <p className="underscore-p">
+                    <h1 className="footer-label">LET'S CONNECT</h1>
+                    <h3 className="footer-sayHello">Say hello at <span className="underscore-p">
                         magnitoshka11@gmail.com
-                    </p></h3>
-                    <h3>For more info, here`s my
-                        <a href="https://drive.google.com/file/d/16EttXAo0fF1gqd91Yn0lYRW7-QtgKFpK/view?usp=sharing">
-                            <p className="underscore-p">resume</p>
-                        </a>
+                    </span></h3>
+                    <h3 className="footer-sayHello">For more info, here`s my <a
+                        href="https://drive.google.com/file/d/16EttXAo0fF1gqd91Yn0lYRW7-QtgKFpK/view?usp=sharing">
+                        <span className="underscore-p">resume</span></a>
                     </h3>
                     <div className="footer-nav-icons">
                         <div className="footer-nav-LinkedId"></div>
